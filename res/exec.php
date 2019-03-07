e4dc5e23c176f2c6e172dcc5aef48618b10ec8f7
siaer\stagistaancona1 <federicosimonetti94@gmail.com>
2019-03-07 10:38:18 +0100

    Aggiunte cose

--- res/exec.php
+++ res/exec.php
@@ -1,7 +1,7 @@
 <?php
 $output = array();
 ini_set('max_execution_time', 300);
-exec("java -jar Solver.jar 10 4 2 results/10_4_2.txt", $output); 
+exec("java -jar Solver.jar 200 8 2 results/200_8_2.txt", $output); 
 echo "<pre>";
 print_r($output);
 ?>
